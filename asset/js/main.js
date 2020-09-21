87c8cc78a067e30b3f052a6ebf477ca7cb3aba1a
twsau <tw.saunders@protonmail.com>
2020-09-21 02:09:58 +0100

    custom fonts and minor imporvements

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,19 +1,42 @@
-const Application = PIXI.Application;
+const {Application, Container, Graphics, Sprite, Text, TilingSprite} = PIXI;
 const loader = PIXI.Loader.shared;
 
 class Game {
 	constructor() {
-		PIXI.utils.skipHello();
-		this.app = new Application({antialias: true, width: 400, height: 400});
+		this.app = Game.#init.app();
 		$('#loading').remove();
 		document.body.appendChild(this.app.view);
 		this.app.ticker.add(delta => this.update(delta));
 	}
-	update() {
+	update(delta) {}
+	static #init = {
+		app: () => {
+			PIXI.utils.skipHello();
+			return new Application(CONFIG.app);
+		}
+	}
+}
 
+const CONFIG = {
+	app: {
+		antialias: false,
+		height: window.innerHeight,
+		transparent: false,
+		width: window.innerWidth
 	}
 }
 
 loader
 	// .add('sprite alias', 'filepath')
-	.load(function(){$(document).ready(function() {game = new Game();});});
+	.load(function(){
+		$(document).ready(function() {
+			WebFont.load({
+	        	google: {
+	            	families: ['Press Start 2P']
+	        	},
+	        	active:e=>{
+	            	game = new Game();
+	        	}
+	    	});
+		});
+	});
