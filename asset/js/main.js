54d8efea3e5d0be3fa8e6e366dadfb5d644aaee1
twsau <tw.saunders@protonmail.com>
2020-10-03 15:32:36 +0100

    responsive screen size added

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,42 +1,29 @@
-const {Application, Container, Graphics, Sprite, Text, TilingSprite} = PIXI;
+const {Application, Text} = PIXI;
 const loader = PIXI.Loader.shared;
 
 class Game {
 	constructor() {
-		this.app = Game.#init.app();
+		PIXI.utils.skipHello();
+		this.app = new Application(CONFIG.app);
 		$('#loading').remove();
 		document.body.appendChild(this.app.view);
 		this.app.ticker.add(delta => this.update(delta));
 	}
-	update(delta) {}
-	static #init = {
-		app: () => {
-			PIXI.utils.skipHello();
-			return new Application(CONFIG.app);
-		}
+	update(delta) {
+
 	}
 }
 
 const CONFIG = {
 	app: {
-		antialias: false,
-		height: window.innerHeight,
+		antialias: true,
+		backgroundColor: 0x161925,
+		height: 768,
 		transparent: false,
-		width: window.innerWidth
+		width: 1366
 	}
 }
 
 loader
 	// .add('sprite alias', 'filepath')
-	.load(function(){
-		$(document).ready(function() {
-			WebFont.load({
-	        	google: {
-	            	families: ['Press Start 2P']
-	        	},
-	        	active:e=>{
-	            	game = new Game();
-	        	}
-	    	});
-		});
-	});
+	.load(function(){$(document).ready(function() {WebFont.load({google: {families: ['Press Start 2P']},active:e=>{game = new Game();}});});});
